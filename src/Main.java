22b0db9e691edcbcbdadf8783671050c98fc9668
Unicronic1 <egor.zolotarev.06@list.ru>
2025-10-04 18:11:19 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args){
+
+        System.out.println("Hello");
+
+        short age = 120;
+        System.out.println(age);
+
+        float num = 4.555f;
+        System.out.println(num);
+
+        char ch = 't';
+        String name = "Monday";
+        System.out.println(name);
+
+
+    }
+}
